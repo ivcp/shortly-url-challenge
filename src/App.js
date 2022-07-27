95d1a725807f363b1d9ac7ce01a5935f685f441e
ivcp <blizujemars@gmail.com>
2022-07-27 11:48:05 +0200

    shorten media queries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/layout/Header';
 import ShortenInput from './components/ShortenInput';
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
       <Header />
-      <main>
+      <main className={styles.main}>
         <ShortenInput />
       </main>
     </>
--- src/components/ShortenInput.js
+++ src/components/ShortenInput.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button';
 import Grid from './layout/Grid';
 import Link from './Link';
 import styles from './ShortenInput.module.css';
 
 const ShortenInput = () => {
+  const [error, setError] = useState(false);
+
+  const submitHandler = e => {
+    //TODO:
+    e.preventDefault();
+  };
+
   return (
     <section className={styles.shorten}>
       <Grid>
         <div className={styles.container}>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={submitHandler}>
             <input
               className={styles.input}
               type="text"
               placeholder="Shorten a link here..."
             />
+            {error && <p>Please add a link</p>}
             <Button submit shorten>
               Shorten it!
             </Button>
@@ -28,3 +36,7 @@ const ShortenInput = () => {
 };
 
 export default ShortenInput;
+
+//TODO:
+// validate input (not empty, is link)
+//
