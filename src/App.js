3fa677e74b3dd3eb3b1ea29d0a47e030ab83795e
ivcp <blizujemars@gmail.com>
2022-07-27 21:50:24 +0200

    boost section done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/layout/Header';
 import ShortenInput from './components/ShortenInput';
 import styles from './App.module.css';
 import AdvancedStatistics from './components/layout/AdvancedStatistics';
+import Boost from './components/layout/Boost';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main className={styles.main}>
         <ShortenInput />
         <AdvancedStatistics />
+        <Boost />
       </main>
     </>
   );
---
+++ src/components/layout/Boost.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../../UI/Button';
+import styles from './Boost.module.css';
+
+const Boost = () => {
+  return (
+    <section className={styles.boost}>
+      <h1>Boost your links today</h1>
+      <Button>Get started</Button>
+    </section>
+  );
+};
+
+export default Boost;
